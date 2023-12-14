c2d5f32ce2421310eefc486357505c20f325f696
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-12-14 21:34:41 +0100

    crearetNewUniversityMethod

--- src/controllers/Universities.js
+++ src/controllers/Universities.js
@@ -2,6 +2,30 @@ const { University } = require("../models");
 
 const createUniversity = async (req, res) => {
   try {
+    const data = [req.body.name, req.body.description, req.body.country_id];
+    const image = req.file.filename;
+    const universityExists = await University.findOne({
+      name: data[0],
+      country_id: data[2],
+    });
+    if (universityExists) {
+      res.status(200).send({
+        message: "This university already exists!",
+        university: universityExists,
+      });
+    } else {
+      const newUniversity = await University.create({
+        name: data[0],
+        description: data[1],
+        country_id: data[2],
+        image,
+      });
+      if (newUniversity) {
+        res.status(200).send(newUniversity);
+      } else {
+        res.status(500).send("University doesn't created!");
+      }
+    }
   } catch (error) {
     res.status(500).send(error.message);
   }
